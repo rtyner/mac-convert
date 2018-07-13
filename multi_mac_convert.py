5b8edd206280afd550fdffb3c78a09611ace8c71
rtyner <rusty@rtyner.com>
2018-07-13 06:34:56 -0400

    added for loop

--- multi_mac_convert.py
+++ multi_mac_convert.py
@@ -1,33 +1,45 @@
-# Rusty Tyner
-# 7/10/18
-# Take MAC address file as input and convert to different formats
-
-# TODO:
-# Loop through input file, output to csv, prompt for MAC type to convert to
+###################################################################################
+# Rusty Tyner                                                                     #
+# 7/10/18                                                                         #
+# Take MAC address file as input and convert to different formats                 #
+#                                                                                 #  
+# TODO: Loop through input file, output to csv, prompt for MAC type to convert to #
+###################################################################################
 
 import sys
 import netaddr
 from netaddr import EUI
 
+# take file as input
 in_file = sys.argv[1]
 # out_file = sys.argv[2]
 
 def convert_mac(octet):
     return EUI(netaddr.strategy.eui48.packed_to_int(octet))
 
+# open and read file
 with open(in_file,'r') as i:
     lines = i.readlines()
+    for line in lines:
+        #print(line, end="")
+        mac = EUI(line)
+        print(line, end="")
+# in this code block the list is being read with \n at the end of each line, need to remove that.
+# netaddr.core.AddrFormatError: failed to detect EUI version: '13:1B:E5:CC:E6:B3    \n'
 
-in_mac = lines
 
-mac = EUI(in_mac[1])
-print(mac)
 
-mac.dialect = netaddr.mac_cisco
-print(mac)
+# store input from file into in_mac variable
+#in_mac = lines
 
-mac.dialect = netaddr.mac_bare
-print(mac)
+#mac = EUI(in_mac[1])
+#print(mac)
 
-mac.dialect = netaddr.mac_unix_expanded
-print(mac)
+#mac.dialect = netaddr.mac_cisco
+#print(mac)
+
+#mac.dialect = netaddr.mac_bare
+#print(mac)
+
+#mac.dialect = netaddr.mac_unix_expanded
+#print(mac)
