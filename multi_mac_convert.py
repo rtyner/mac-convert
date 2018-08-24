9160eac1d50f9f45b2010ce6d7be6284a1a06ab6
rtyner <rusty@rtyner.com>
2018-08-24 16:47:33 -0400

    trying to unscrew my past mistakes ;_;

--- multi_mac_convert.py
+++ multi_mac_convert.py
@@ -21,11 +21,9 @@ def convert_mac(octet):
 with open(in_file,'r') as i:
     lines = i.readlines()
     for line in lines:
-        #print(line, end="")
-        mac = EUI(line)
-        print(line)
-# in this code block the list is being read with \n at the end of each line, need to remove that.
-# netaddr.core.AddrFormatError: failed to detect EUI version: '13:1B:E5:CC:E6:B3    \n'
+#        mac = EUI(line)
+        print(line) # in this code block the list is being read with \n at the end of each line, need to remove that.
+                    # netaddr.core.AddrFormatError: failed to detect EUI version: '13:1B:E5:CC:E6:B3    \n'
 
 
 
