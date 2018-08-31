4710d8d96b605162a459a79fd6bebd40cd17bddb
rtyner <rusty@rtyner.com>
2018-08-30 20:34:47 -0400

    added input for mac type, and successfully converted infile to bare, cisco, unix, and eui48

--- multi_mac_convert.py
+++ multi_mac_convert.py
@@ -1,6 +1,17 @@
+# types of macs to convert from/to
+# eui48 - 71-54-ED-69-9B-E9
+# unix - 71:54:ed:69:9b:e9
+# cisco - 7154.ed69.9be9
+# bare - 7154ED699BE9
+
 import sys
 import netaddr
 from netaddr import EUI
+
+# take certain input here, and be able to act on it. 
+# if mac_type == 'Cisco':
+#   print(netaddr.mac_cisco) - something like that, not sure on the syntax yet
+mac_type = input("What format is the input file? Cisco, Bare, Unix, or ")
 
 # take file as input
 in_file = sys.argv[1]
@@ -8,10 +19,21 @@ in_file = sys.argv[1]
 def convert_mac(octet):
     return EUI(netaddr.strategy.eui48.packed_to_int(octet))
 
-# open and read file
-with open(in_file,'r') as i:
+with open(in_file,'r') as i: # open and read file
     lines = i.readlines()
     for line in lines:
         mac = EUI(line)
+        
+        # would like to devise a way to clean up and condense this block below
         mac.dialect = netaddr.mac_bare
-        print(line) 
+        print(mac) 
+
+        mac.dialect = netaddr.mac_cisco
+        print(mac) 
+
+        mac.dialect = netaddr.mac_unix_expanded
+        print(mac) 
+        
+        mac.dialect = netaddr.mac_eui48
+        print(mac) 
+
